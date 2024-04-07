7139c00d0ede6e59045fdf8d89620d994398739d
ringinrp <ringinrp21@gmail.com>
2024-04-07 14:27:07 +0700

    update

---
+++ dist/test/hello.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe('Hello', function () {
+    it("should say hello", function () {
+        const name = "Ringin Restu Pati";
+        expect(name).toBe("Ringin Restu Pati"); //untuk menjalankannya npx jest test/hello.test.ts
+    });
+});
---
+++ dist/test/say-hello.test.js
@@ -0,0 +1,6 @@
+import { sayHello } from "../src/say-hello";
+describe('sayHello', function () {
+    it('should return hello ringin', function () {
+        expect(sayHello('ringin')).toBe('hello ringin');
+    });
+});
